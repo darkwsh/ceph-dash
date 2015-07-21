4ac7889b0b899229b5b109cfea3a214fba0a3a8e
darkwsh <darkwsh@launchpad.(none)>
2015-07-21 18:11:11 +0800

    Add 5 min task to monitor osd and send email when error.

---
+++ mail.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import smtplib
+import time
+import ConfigParser
+import string, os, sys
+
+from email.mime.text import MIMEText
+import ceph_dash
+from ceph_dash import CephApiConfig
+from ceph_dash import CephClusterProperties
+from ceph_dash import CephClusterCommand
+from rados import Rados
+from rados import ObjectNotFound
+from rados import PermissionError
+from rados import Error as RadosError
+
+class MailSender:
+    mail_user=''
+    mail_pass=''
+    mail_host="smtp.qq.com"
+    mailto_list=[]
+
+    def __init__(self, name, password):
+        self.mail_user=name
+	self.mail_pass=password
+
+    def set_mailto(self, mailto):
+	print mailto
+        self.mailto_list=mailto
+
+    def send_mail(self,context):
+        me="<"+self.mail_user+">"
+	msg=MIMEText(context,_subtype='html',_charset='gb2312')
+	msg['Subject']='CEPH WARNING'
+	msg['From']=me
+	msg['To']=";".join(self.mailto_list)
+	try:
+            s=smtplib.SMTP()
+	    s.connect(self.mail_host)
+	    s.login(self.mail_user,self.mail_pass)
+	    s.sendmail(me, self.mailto_list, msg.as_string())
+	    s.close()
+	    return True
+        except Exception, e:
+	    print str(e)
+	    return False
+
+class MailMonitor:
+
+    def __init__(self):
+        self.config = CephApiConfig()
+        self.clusterprop = CephClusterProperties(self.config)
+    
+    def get(self):
+	with Rados(**self.clusterprop) as cluster:
+	    cluster_status = CephClusterCommand(cluster, prefix='status', format='json')
+       	    if 'err' in cluster_status:
+	        return 'Can not get the ceph cluster status!'
+
+            total_osds = cluster_status['osdmap']['osdmap']['num_osds']
+	    in_osds = cluster_status['osdmap']['osdmap']['num_up_osds']
+	    up_osds = cluster_status['osdmap']['osdmap']['num_in_osds']
+
+	    if up_osds < total_osds or in_osds < total_osds:
+	        osd_status = CephClusterCommand(cluster, prefix='osd tree', format='json')
+	        if 'err' in osd_status:
+                    return 'Can not get the osd status!'
+
+                cluster_status['osdmap']['details'] = ceph_dash.get_unhealthy_osd_details(osd_status)
+	        return cluster_status['osdmap']['details']
+	    else:
+		return '';
+
+
+
+if __name__ == '__main__':
+    mon = MailMonitor()
+    cf = ConfigParser.ConfigParser()
+    cf.read("mail.conf")
+    user_name = cf.get("mail", "user_name")
+    pass_word = cf.get("mail", "pass_word")
+    mailto_list = cf.get("mail", "mailto").split(",")
+    mail_sender = MailSender(user_name, pass_word)
+    mail_sender.set_mailto(mailto_list)
+
+    while True:
+        osd_status = mon.get()
+        if osd_status != '':
+    	    context = "<table>"
+	    context += "<tr>"
+	    context += "<td style='width:100px'>status</td>"
+	    context += "<td style='width:100px'>host</td>"
+	    context += "<td style='width:100px'>name</td>"
+	    context += "</tr>"
+	    if type(osd_status)==list:
+  	        for item in osd_status:
+		    context += "<tr><td>"+item['status']+"</td><td>"+item['host']+"</td><td>"+item['name']+"</td></tr>"
+	    else:
+		context += "<tr><td colspan='3'>"+str(osd_status)+"</td></tr>"
+	    context += "</table>"
+	    mail_sender.send_mail(str(context))
+	time.sleep(300)
